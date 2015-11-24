b0eb98e1e744d8723f05634757e2f970a53924fc
Talon Daniels <talondaniels@gmail.com>
2015-11-23 21:25:41 -0700

    Remove duplicate test

--- test/index.js
+++ test/index.js
@@ -128,13 +128,4 @@ describe('Mocked Hiscore API', function () {
         });
     });
 
-    it('can communicate with Jagex servers', function (done) {
-        this.slow(1000);
-        nock.cleanAll();
-        hiscore.standard('king_karthas', function (err, data) {
-            expect(err).to.be.a('null');
-            expect(data).to.not.be.a('null');
-            done();
-        });
-    });
 });
