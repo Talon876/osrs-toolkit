b76e5d1e7a4e4f4bec838ad5b736245a0c6914e8
Talon Daniels <talondaniels@gmail.com>
2015-11-23 20:30:37 -0700

    Add code coverage

--- test/index.js
+++ test/index.js
@@ -94,4 +94,14 @@ describe('Mocked Hiscore API', function () {
             done();
         });
     });
+
+    it('can communicate with Jagex servers', function (done) {
+        this.slow(1000);
+        nock.cleanAll();
+        hiscore.standard('king_karthas', function (err, data) {
+            expect(err).to.be.a('null');
+            expect(data).to.not.be.a('null');
+            done();
+        });
+    });
 });
